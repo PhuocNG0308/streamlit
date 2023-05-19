08f82a12adaee0971d0052ffddf7a5d83bc42ece
PhuocNG0308 <71546982+PhuocNG0308@users.noreply.github.com>
2023-05-19 22:04:21 +0700

    Update test_0.py

--- test_0.py
+++ test_0.py
@@ -3,3 +3,8 @@ st.text('hello, world')
 st.text('hellooooooooooooooooo, world')
 
 st.form('my_form_identifier')
+st.form_submit_button('Submit to me')
+st.container()
+st.columns(spec)
+col1, col2 = st.columns(2)
+col1.subheader('Columnisation')
