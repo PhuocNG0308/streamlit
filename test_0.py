953d5d08c1e71fe0d15d6d8db6415212fccc1bf3
PhuocNG0308 <71546982+PhuocNG0308@users.noreply.github.com>
2023-05-24 11:03:16 +0700

    Push all

--- test_0.py
+++ test_0.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
 import time
+
+def check_login(username, password):
+    # Đọc dữ liệu từ file CSV
+    df = pd.read_csv('UserData.csv')
+
+    # Kiểm tra username và password
+    if username in df['username'].values and password in df['password'].values:
+        return True
+    else:
+        return False
+
 
 def main():
     # Biến trạng thái
@@ -13,32 +25,13 @@ def main():
 
     if login_button:
         # Kiểm tra đăng nhập
-        if username == "admin" and password == "password":
+        if check_login(username, password):
             # Đăng nhập thành công, đặt biến trạng thái là True
             logged_in = True
 
     # Kiểm tra trạng thái đăng nhập
     if logged_in:
-        # Hiển thị menu thao tác
-        show_menu()
-
-def show_menu():
-    st.title("Menu thao tác")
-    st.write("Chọn một tác vụ từ menu bên dưới.")
-
-    # Hiển thị các tác vụ trong menu
-    menu_options = ["Tác vụ 1", "Tác vụ 2", "Tác vụ 3"]
-    selected_option = st.selectbox("Chọn tác vụ", menu_options)
-
-    if selected_option == "Tác vụ 1":
-        # Xử lý tác vụ 1
-        st.write("Đang xử lý tác vụ 1...")
-    elif selected_option == "Tác vụ 2":
-        # Xử lý tác vụ 2
-        st.write("Đang xử lý tác vụ 2...")
-    elif selected_option == "Tác vụ 3":
-        # Xử lý tác vụ 3
-        st.write("Đang xử lý tác vụ 3...")
+        st.write("Bruh!!")
 
 if __name__ == "__main__":
     main()
