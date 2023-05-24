4517167c5404d41007db1b73d4aa6d34f6b8e526
PhuocNG0308 <71546982+PhuocNG0308@users.noreply.github.com>
2023-05-24 10:14:39 +0700

    Update test_0.py

--- test_0.py
+++ test_0.py
@@ -1,17 +1,37 @@
 import streamlit as st
 
 def main():
+    # Panel đăng nhập
     st.title("Đăng nhập")
-
-    username = st.text_input("Tên người dùng")
+    username = st.text_input("Tên đăng nhập")
     password = st.text_input("Mật khẩu", type="password")
+    login_button = st.button("Đăng nhập")
 
-    if st.button("Đăng nhập"):
+    if login_button:
+        # Kiểm tra đăng nhập
         if username == "admin" and password == "password":
-            st.success("Đăng nhập thành công")
-            # Thực hiện các hành động sau khi đăng nhập thành công
+            # Hiển thị menu thao tác
+            show_menu()
         else:
-            st.error("Tên người dùng hoặc mật khẩu không chính xác")
+            st.error("Tên đăng nhập hoặc mật khẩu không chính xác.")
+
+def show_menu():
+    st.title("Menu thao tác")
+    st.write("Chọn một tác vụ từ menu bên dưới.")
+
+    # Hiển thị các tác vụ trong menu
+    menu_options = ["Tác vụ 1", "Tác vụ 2", "Tác vụ 3"]
+    selected_option = st.selectbox("Chọn tác vụ", menu_options)
+
+    if selected_option == "Tác vụ 1":
+        # Xử lý tác vụ 1
+        st.write("Đang xử lý tác vụ 1...")
+    elif selected_option == "Tác vụ 2":
+        # Xử lý tác vụ 2
+        st.write("Đang xử lý tác vụ 2...")
+    elif selected_option == "Tác vụ 3":
+        # Xử lý tác vụ 3
+        st.write("Đang xử lý tác vụ 3...")
 
 if __name__ == "__main__":
     main()
